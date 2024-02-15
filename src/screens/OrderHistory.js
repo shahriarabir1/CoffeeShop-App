fb246ad1a09a1c5859ceded459945bb230ede929
shahriarabir1 <shahriarabirjob@gmail.com>
2024-02-15 21:12:45 +0600

    OrderHistory

--- src/screens/OrderHistory.js
+++ src/screens/OrderHistory.js
@@ -1,14 +1,136 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { useStore } from "../stores/store";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from "../theme/theme";
+import HeaderBar from "../components/HeaderBar";
+import EmptyAnimation from "../components/EmptyAnimation";
+import PopUp from "../components/Popup";
+import OrderHistoryCard from "../components/OrderHistoryCard";
 
-const OrderHistory = () => {
+const OrderHistoryScreen = ({ navigation }) => {
+  const OrderHistoryList = useStore((state) => state.OrderHistoryList);
+  const tabBarHeight = useBottomTabBarHeight();
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  const navigationHandler = ({ item }) => {
+    navigation.push("Details", {
+      item: item,
+    });
+  };
+
+  const buttonPressHandler = () => {
+    setShowAnimation(true);
+    setTimeout(() => {
+      setShowAnimation(false);
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>OrderHistory</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      {showAnimation ? (
+        <PopUp
+          style={styles.LottieAnimation}
+          source={require("../lottie/download.json")}
+        />
+      ) : (
+        <></>
+      )}
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
+        <View
+          style={[styles.ScrollViewInnerView, { marginBottom: tabBarHeight }]}
+        >
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Order History" />
+
+            {OrderHistoryList.length == 0 ? (
+              <EmptyAnimation title={"No Order History"} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {OrderHistoryList.map((data, index) => (
+                  <OrderHistoryCard
+                    key={index.toString()}
+                    navigationHandler={navigationHandler}
+                    CartList={data.CartList}
+                    CartListPrice={data.CartListPrice}
+                    OrderDate={data.OrderDate}
+                  />
+                ))}
+              </View>
+            )}
+          </View>
+          {OrderHistoryList.length > 0 ? (
+            <TouchableOpacity
+              style={styles.DownloadButton}
+              onPress={() => {
+                buttonPressHandler();
+              }}
+            >
+              <Text style={styles.ButtonText}>Download</Text>
+            </TouchableOpacity>
+          ) : (
+            <></>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-export default OrderHistory;
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  LottieAnimation: {
+    height: 250,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_30,
+  },
+  DownloadButton: {
+    margin: SPACING.space_20,
+    backgroundColor: COLORS.primaryOrangeHex,
+    alignItems: "center",
+    justifyContent: "center",
+    height: SPACING.space_36 * 2,
+    borderRadius: BORDERRADIUS.radius_20,
+  },
+  ButtonText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default OrderHistoryScreen;
